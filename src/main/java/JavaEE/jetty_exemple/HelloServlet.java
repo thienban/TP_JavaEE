206a4db53390c32d902818d85ff87305ec41a28f
thien ban BUI <thienban91@hotmail.fr>
2017-12-13 10:39:17 +0100

    first commit

---
+++ src/main/java/JavaEE/jetty_exemple/HelloServlet.java
@@ -0,0 +1,17 @@
+package JavaEE.jetty_exemple;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse rep) throws ServletException, IOException {
+
+		PrintWriter writer = rep.getWriter();
+		writer.write("Hello");
+	}
+}
